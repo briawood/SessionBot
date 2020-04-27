f8ca63847ae4bde53eac21b72c03c74abf835ea7
Brian <brian@briansmusic.com>
2020-04-27 10:46:43 -0600

    Update
    fix commands

--- commands/kick.js
+++ commands/kick.js
@@ -1,10 +1,9 @@
 module.exports = (message, name) => {
-//    var msg = "@here Please vote to kick " + (res[1] || "") + " and confirm by clicking the boot."
-    var msg = "Please vote to kick " + (name || "") + " and confirm by clicking the boot."
-    message.channel.send(msg).then(function (reply) {
-      reply.react("👢")
-      message.delete()
-    }).catch(function() {
-      //Something
-    })
+  var msg = "Please vote to kick **" + (name || "") + "** and confirm by clicking the boot."
+  message.channel.send(msg).then(function (reply) {
+    reply.react("👢")
+    message.delete()
+  }).catch(function() {
+    //Something
+  })
 }
--- events/message.js
+++ events/message.js
@@ -1,4 +1,5 @@
 const kick = require("../commands/kick")
+const sell = require("../commands/sell")
 
 module.exports = (client, message) => {
     var res = message.content.split(" ")
@@ -6,7 +7,6 @@ module.exports = (client, message) => {
         return kick(message, res[1])
     }
     if (res[0] === "!sell") {
-  //    message.guild.channel.get("703332387961962637").send("Message")
-  //    bot.guilds.get('689154684380119069').channels.get('703332387961962637').send("Hello World!")
+      return sell(message, res[1])
     }
 }
